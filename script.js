0483eb5c546bac8c817747407b6fc87267cc7474
Umberto Carlo de Matteis <umberto.carlo@gmail.com>
2021-12-30 19:44:28 +0100

    submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function compute()
 {
+	 var result = document.getElementById("result");
+	result.innerHTML="";
     if (!validate())
     {
         return;
@@ -10,7 +12,7 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 
-    var result = document.getElementById("result");
+   
     result.innerHTML="If you deposit <mark>"+principal+"</mark><br/>"+
     "at an interest rate of <mark>"+rate+"%.</mark><br/>"+
     "You will receive an amount of <mark>"+interest+"</mark><br/>"+
